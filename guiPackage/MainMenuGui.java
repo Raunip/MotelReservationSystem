f6160c0f6123ab47cce40a32da7178ebcca7090a
Raunip <101034067+Raunip@users.noreply.github.com>
2022-11-24 22:43:38 -0600

    added MainMenu GUI to guiPackage

---
+++ guiPackage/MainMenuGui.java
@@ -0,0 +1,41 @@
+package guiPackage;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenuGui {
+
+    private  static final JPanel panel = new JPanel();
+    private static final String reservation = "Reservation";
+    private static final String avail_rooms = "Find Available Rooms";
+    private static final String check_in = "Check In";
+    private static final String check_out = "Check Out";
+
+
+    public static void main (String[] args){
+        JFrame frame = new JFrame("Main Menu");
+        frame.setSize(300, 400);
+        frame.setLayout(null);
+
+        JButton res_button = new JButton(reservation);
+        res_button.setBounds(90, 40, 100, 40);
+
+        JButton find_roomsButton = new JButton(avail_rooms);
+        find_roomsButton.setBounds(90,100,100,40);
+
+        JButton check_inButton = new JButton(check_in);
+        check_inButton.setBounds(90,160,100,40);
+
+        JButton check_outButton = new JButton(check_out);
+        check_outButton.setBounds(90,220,100,40);
+
+        frame.add(res_button);
+        frame.add(find_roomsButton);
+        frame.add(check_inButton);
+        frame.add(check_outButton);
+        panel.setBackground(new Color(0, 119, 200));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
