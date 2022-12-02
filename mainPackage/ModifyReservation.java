c2d43d517171f70e7482a8e3c6db9ba324515ef3
Raunip <101034067+Raunip@users.noreply.github.com>
2022-12-02 11:35:06 -0600

    Created setter to modify the data

--- mainPackage/ModifyReservation.java
+++ mainPackage/ModifyReservation.java
@@ -1,16 +1,87 @@
-package mainPackage;// insert package name here
+package mainPackage;
 
-public class ModifyReservation
+/**
+ *  This class overrides the setters of MakeReservation to modify the information
+ */
+public class ModifyReservation extends MakeReservation
 {
-    private int confirmationNum;
 
-    public ModifyReservation(int num)
-    {
-        confirmationNum = num;
+    /**
+     * Modifies the customer info
+     * @param firstname first name of customer
+     * @param lastname  last name  of customer
+     * @param address   address of customer
+     * @param zipcode    zip code of customer
+     * @param payment   payment method of customer
+     */
+    public ModifyReservation(String firstname, String lastname, String address, Integer zipcode, String payment) {
+        super(firstname, lastname, address, zipcode, payment);
     }
 
-    void changeReservation(/* insert necessary variables */)
-    {
-        // insert necessary code
+    /**
+     * changes the first name
+     * @param firstname first name of customer
+     */
+    @Override
+    public void setFirstname(String firstname) {
+        super.setFirstname(firstname);
+    }
+
+    /**
+     * changes the last name
+     * @param lastname last name  of customer
+     */
+    @Override
+    public void setLastname(String lastname) {
+        super.setLastname(lastname);
+    }
+
+    /**
+     *changes the address
+     * @param address address of customer
+     */
+    @Override
+    public void setAddress(String address) {
+        super.setAddress(address);
+    }
+
+    /**
+     * changes the zipcode
+     * @param zipcode zip code of customer
+     */
+    @Override
+    public void setZipcode(Integer zipcode) {
+        super.setZipcode(zipcode);
+    }
+
+    /**
+     * changes the payment method
+     * @param payment payment method of customer cash or credit
+     */
+    @Override
+    public void setPayment(String payment) {
+        super.setPayment(payment);
+    }
+
+    /**
+     * changes check in date
+     * @param month month
+     * @param day   day
+     * @param year  year
+     */
+    @Override
+    public void setCheck_in_date(int month, int day, int year) {
+        super.setCheck_in_date(month, day, year);
+    }
+
+    /**
+     * changes check out date
+     * @param month month
+     * @param day day
+     * @param year year
+     */
+    @Override
+    public void setCheck_out_date(int month, int day, int year) {
+        super.setCheck_out_date(month, day, year);
     }
 }
